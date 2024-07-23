a475f7033692d9015fe490ebce3fdee8ba324328
Marius Riis Haugan <mariusthedev@gmail.com>
2024-07-23 11:32:32 +0200

    add JWT signing

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,4 +1,10 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const path = require('path')
+const fsPromises = require('fs').promises
+
+const USERS_FILEPATH = path.join(__dirname, '..', 'models', 'users.json')
 
 const usersDatabase = {
     users: require('../models/users.json'),
@@ -24,7 +30,23 @@ const loginUser = async (req, res) => {
     // Evaluate password
     const matchedPassword = await bcrypt.compare(password, foundUser.password)
     if (matchedPassword) {
-        // Create JWTs for access and refresh
+        // Create JWTs
+        const accessToken = jwt.sign({
+            "username": foundUser.username
+        }, process.env.ACCESS_SECRET, {
+            expiresIn: '10s' // TODO: set to 15 minutes
+        })
+        const refreshToken = jwt.sign({
+            "username": foundUser.username
+        }, process.env.REFRESH_SECRET, {
+            expiresIn: '30s' // TODO: set to one day
+        })
+        // Write updates to simulated DB (JSON file)
+        const otherUsers = usersDatabase.users.filter(item => item.username !== foundUser.username)
+        const currentUserWithRefreshToken = { ...foundUser, refreshToken }
+        usersDatabase.initializeUserData([...otherUsers, currentUserWithRefreshToken])
+        await fsPromises.writeFile(USERS_FILEPATH, JSON.stringify(usersDatabase.users))
+        // Return to caller
         return res.json({
             "message": `User ${username} logged in!`
         })

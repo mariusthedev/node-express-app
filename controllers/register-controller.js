bd9b620200b59d1b03a15016544da094e5f667ea
Marius Riis Haugan <mariusthedev@gmail.com>
2024-07-23 10:44:01 +0200

    Register user backend functionality

---
+++ controllers/register-controller.js
@@ -0,0 +1,50 @@
+const fsPromises = require('fs').promises
+const path = require('path')
+const bcrypt = require('bcrypt')
+
+const USERS_FILEPATH = path.join(__dirname, '..', 'models', 'users.json')
+
+const userDatabase = {
+    users: require('../models/users.json'),
+    initializeUserData: function (data) {
+        this.users = data
+    }
+}
+
+const registerUser = async (req, res) => {
+    const {
+        username, 
+        password 
+    } = req.body
+    if (!username || !password) {
+        return res.status(400).json({
+            "message": "Username and/or password was not provided!"
+        })
+    }
+    // Check for duplicate usernames
+    const duplicate = userDatabase.users.find(item => item.username === username)
+    if (duplicate) {
+        return res.sendStatus(409) // Conflict
+    }
+    try {
+        // Encrypt the password
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = {
+            "username": username,
+            "password": hashedPassword
+        }
+        // Store the user in simulated DB (write to JSON file)
+        userDatabase.initializeUserData([...userDatabase.users, newUser])
+        await fsPromises.writeFile(USERS_FILEPATH, JSON.stringify(userDatabase.users))
+        console.log(userDatabase.users)
+        return res.status(201).json({
+            "message": `New user ${username} was created!`
+        })
+    } catch (error) {
+        return res.status(500).json({
+            "message": error.message
+        })
+    }
+}
+
+module.exports = { registerUser } 
